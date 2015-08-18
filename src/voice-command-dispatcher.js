1a793b6553e419e53a126620764a2ff141c2aa64
Inti Piolanti <Inti_Piolanti@intuit.com>
2015-08-17 21:15:29 -0300

    Added registry functionality

--- src/voice-command-dispatcher.js
+++ src/voice-command-dispatcher.js
@@ -1,9 +1,68 @@
+var MessageRegistry = function() {
+
+  /**
+   * A pairing 'message' : [component1, ..., componentN], matching a message to an
+   * array of listening components
+   * @type {Object}
+   */
+  var register = {};
+
+  /**
+   * Adds a component to the registry, so it can be notified when the given message is received
+   * @param  {Object} component The web component
+   * @param  {String} message   A message that will be associated with an action
+   */
+  function subscribe(component, message) {
+    if (register[message]) {
+      register[message].push(component);
+    } else {
+      register[message] = [];
+      register[message].push(component);
+    }
+  }
+
+  /**
+   * Removes a component from a given message queue
+   * @param  {Object} component The web component
+   * @param  {String} message   The message to stop listening to
+   */
+  function unsubscribe(component, message) {
+    var index;
+    if (register[message]) {
+      index = register[message].indexOf(component);
+      if (index !== -1) {
+        register[message].splice(index, 1);
+      }
+    }
+  }
+
+  /**
+   * Notifies all componentes waiting for the received message
+   * @param  {String} message The received message
+   */
+  function notify(message) {
+    var index, length;
+    if (register[message]) {
+      length = register[message].length;
+      for(index = 0; index <= length; index ++) {
+        register[message][index].notify(message);
+      }
+    }
+  }
+}
+
 var VoiceCommandDispatcher = function() {
 
   /**
    * The AudioContext to be used in every step of the voice command dispatcher flow
    */
   var audioContext;
+
+  /**
+   * The registry containing all the dispatch information
+   */
+  var registry;
+
   /**
    * A constant representing the sample rate
    * @type {Number}
@@ -129,7 +188,7 @@ var VoiceCommandDispatcher = function() {
   /**
    * Initializes the capture of sound provenient from the microphone, if supported.
    */
-  function initialize() {
+  function initializeAudioCapture() {
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
                     navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
@@ -144,9 +203,17 @@ var VoiceCommandDispatcher = function() {
     }
   }
 
+  /**
+   * Crates the instance for the message registry
+   */
+  function createRegistry() {
+    registry = new MessageRegistry();
+  }
+
   return {
     start: function() {
-      initialize();
+      createRegistry();
+      initializeAudioCapture();
     }
   }
 }
